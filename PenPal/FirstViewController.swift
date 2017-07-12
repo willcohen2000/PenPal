0bbb251a06ff7a28f46d51744ac7a7aa71c706e8
willcohen2000 <willcohen2000@gmail.com>
2017-07-12 09:14:57 -0400

    Initial commit

---
+++ PenPal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PenPal
+//
+//  Created by Will Cohen on 7/12/17.
+//  Copyright © 2017 Will Cohen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
